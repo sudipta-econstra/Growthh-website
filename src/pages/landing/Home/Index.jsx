9c4a048bc1c88c764f225aff8d078bb0dcf48fde
econstratech <kaushik.econstra@gmail.com>
2024-10-18 17:40:07 +0530

    google translator

--- src/pages/landing/Home/Index.jsx
+++ src/pages/landing/Home/Index.jsx
@@ -59,9 +59,29 @@ function Index() {
                 toast.error(res.response);
             })
     }
+    //translate
+    // const googleTranslateElementInit = () => {
+    //     new window.google.translate.TranslateElement(
+    //       {
+    //         pageLanguage: "en",
+    //         autoDisplay: false
+    //       },
+    //       "google_translate_element"
+    //     );
+    //   };
+    //   useEffect(() => {
+    //     var addScript = document.createElement("script");
+    //     addScript.setAttribute(
+    //       "src",
+    //       "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+    //     );
+    //     document.body.appendChild(addScript);
+    //     window.googleTranslateElementInit = googleTranslateElementInit;
+    //   }, []);
 
     return (
         <>
+            {/* <div id="google_translate_element"></div> */}
             <Drawer drawer={drawer} action={drawerAction.toggle} />
             <HomeOneHeader action={drawerAction.toggle} modalShow={handleShow} />
             <HeroHomeOne modalShow={handleShow} />
